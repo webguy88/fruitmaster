3b4c21b6e20706c6aefe0eb69f6354791abd5ea2
Laurentino <lunaalaurentino@gmail.com>
2020-08-27 23:03:46 -0300

    Add resources
    Manages all the images and media to be loaded, turned into sprite,
    centered, and other properties

---
+++ src/resources.py
@@ -0,0 +1,9 @@
+import pyglet
+from pyglet import resource
+from pyglet import sprite
+
+resource.path = ['../resources', '../resources/art', '../resources/sounds']
+resource.reindex()
+
+black = resource.image('black.png')
+black_spr = sprite.Sprite(black, x=0, y=0)
