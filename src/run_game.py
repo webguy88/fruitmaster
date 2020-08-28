fa30f08ae1eb9535c088e9eda02e459764f36f4a
Laurentino <lunaalaurentino@gmail.com>
2020-08-27 23:14:41 -0300

    Add main file
    The name is run_game, but it works like a main file
    Here:
    - a window is created
    - the camera gets anchored
    - all the drawing and updating issues are implemented
    - instances of certain tools are used as well

---
+++ src/run_game.py
@@ -0,0 +1,94 @@
+import pyglet
+from pyglet import resource
+from pyglet import clock
+from pyglet.window import Window
+from pyglet.window import key
+
+from level1 import Level1, map1
+from engine import Engine
+from camera import Camera
+
+# Resource
+resource.path = ['../resources', '../resources/art', '../resources/sounds']
+resource.reindex()
+
+# Window
+SCREENW = 960
+SCREENH = 640
+window = Window(SCREENW, SCREENH, caption="Fruit Master")
+
+
+# Anchoring camera position
+def position_camera(self, camera: Camera, position: tuple=(0, 0),
+                    zoom: float=1, min_pos: tuple=(None, None),
+                    max_pos: tuple=(None, None)):
+
+        zoom = min(window.width // self.DEFAULT_SIZE[0],
+                   window.height // self.DEFAULT_SIZE[1]) * zoom
+
+        if self.camera.zoom != zoom:
+            self.camera.zoom = zoom
+
+            x = -window.width // 2 // zoom
+            y = -window.height // 2 // zoom
+
+            target_x = position[0]
+            target_y = position[1]
+
+            if min_pos[0] is not None:
+                target_x = max(target_x, min_pos[0])
+            if min_pos[1] is not None:
+                target_y = max(target_y, min_pos[1])
+            if max_pos[0] is not None:
+                target_x = min(target_x, max_pos[0])
+            if max_pos[1] is not None:
+                target_y = min(target_y, max_pos[1])
+
+            x += target_x
+            y += target_y
+
+            if self.camera.position != (x, y):
+                self.camera.position = (x, y)
+
+
+# Window events
+@window.event
+def on_draw():
+    window.clear()
+    camera.begin()
+    engine.draw()
+    camera.end()
+
+
+@window.event
+def on_key_press(symbol, modifiers):
+    engine.on_key_press(symbol, modifiers)
+
+
+@window.event
+def update(dt):
+    engine.update(dt)
+    
+    '''
+    camera.position = (camera.offset_x + vx,
+                       camera.offset_y + vy)
+    '''
+
+
+def is_key_pressed():
+    for _, v in keys.items():
+        if v:
+            return True
+
+    return False
+
+
+keys = key.KeyStateHandler()
+window.push_handlers(keys)
+
+lvl1 = Level1(1, 1, 1, 1, map1)
+engine = Engine(lvl1)
+camera = Camera(scroll_speed=5)
+
+clock.schedule_interval(update, 1/30)
+pyglet.app.run()
